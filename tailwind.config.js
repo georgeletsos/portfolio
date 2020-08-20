08a37eb02ff98c2a1a5149f32c8fa8d95c996a43
georgeletsos <georgeletsos391@gmail.com>
2020-08-20 13:46:02 +0300

    Removed gray colors extension from TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        gray: {
-          ...colors.gray,
-          "100": "#f4f5f7",
-          "200": "#e5e7eb",
-          "400": "#9fa6b2",
-          "500": "#6b7280",
-          "600": "#4b5563",
-          "700": "#374151",
-          "800": "#252f3f",
-          "900": "#161e2e"
-        },
         red: {
           ...colors.red,
           "500": red
